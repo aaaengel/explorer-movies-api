00434706c2d890df2e5ed61338347d1185fab973
Danila Engel <engeldanila@mail.ru>
2021-03-20 16:38:17 +0300

    gone to review

--- controllers/users.js
+++ controllers/users.js
@@ -15,12 +15,12 @@ const returnMe = (req, res, next) => {
 };
 
 const updateUser = (req, res, next) => {
-  const { name, about } = req.body;
+  const { name, email } = req.body;
   User.findByIdAndUpdate(
     req.user._id,
     {
       name,
-      about,
+      email,
     },
     { new: true, runValidators: true },
 
